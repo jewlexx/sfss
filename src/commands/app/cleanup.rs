f143fcd039109daafaed1d5947d8f7484dc0f946
Juliette Cordor <me@cordor.dev>
2024-11-27 14:04:48 +1100

    refactored app collection deciding

--- src/commands/app/cleanup.rs
+++ src/commands/app/cleanup.rs
@@ -1,9 +1,10 @@
 use clap::Parser;
-use sprinkles::{contexts::ScoopContext, packages::reference::package};
-
-use crate::{
-    commands::outdated::apps, handlers::handle_installed_apps, output::colours::eprintln_yellow,
+use sprinkles::{
+    contexts::ScoopContext,
+    packages::reference::{manifest, package},
 };
+
+use crate::{handlers::AppsDecider, output::colours::eprintln_yellow};
 
 #[derive(Debug, Clone, Parser)]
 /// Cleanup apps by removing old versions
@@ -26,7 +27,10 @@ pub struct Args {
 
 impl super::Command for Args {
     async fn runner(self, ctx: &impl ScoopContext) -> anyhow::Result<()> {
-        let cleanup_apps = match handle_installed_apps(ctx, self.all, self.apps)?.as_deref() {
+        let cleanup_apps = match AppsDecider::new(ctx, self.list_apps(), self.apps)
+            .decide()?
+            .as_deref()
+        {
             Some([]) | None => {
                 eprintln_yellow!("No apps selected. Exiting now.");
                 return Ok(());
@@ -35,5 +39,38 @@ impl super::Command for Args {
         };
 
         unimplemented!()
+    }
+}
+
+impl Args {
+    fn list_apps<C: ScoopContext>(
+        &self,
+    ) -> Box<dyn Fn(&C) -> anyhow::Result<Option<Vec<package::Reference>>>> {
+        let all = self.all;
+        Box::new(move |ctx: &C| {
+            if all {
+                let installed_apps: Vec<package::Reference> = {
+                    let installed_apps = ctx.installed_apps()?;
+                    let manifest_paths = installed_apps.into_iter().filter_map(|path| {
+                        let manifest_path = path.join("current").join("manifest.json");
+
+                        manifest_path
+                            .try_exists()
+                            .ok()
+                            .and_then(|exists| exists.then_some(manifest_path))
+                    });
+
+                    let references = manifest_paths
+                        .map(manifest::Reference::File)
+                        .map(manifest::Reference::into_package_ref);
+
+                    references.collect()
+                };
+
+                anyhow::Ok(Some(installed_apps))
+            } else {
+                anyhow::Ok(None)
+            }
+        })
     }
 }
--- src/handlers.rs
+++ src/handlers.rs
@@ -1,44 +1,39 @@
-use std::io;
-
-use sprinkles::{
-    contexts::ScoopContext,
-    packages::reference::{manifest, package},
-};
+use sprinkles::packages::reference::package;
 
 use crate::output::colours::{bright_red, green, yellow};
 
-/// This will get all apps if all is true and no apps were passed.
-///
-/// If all is false and apps are passed, it will only return those apps.
-///
-/// If all is true and apps are passed, it will prompt the user to select which collection to choose.
-///
-/// If the return value is empty, that means no apps were provided and all was false.
-pub fn handle_installed_apps(
-    ctx: &impl ScoopContext,
-    all: bool,
-    apps: Vec<package::Reference>,
-) -> io::Result<Option<Vec<package::Reference>>> {
-    if all {
-        let installed_apps: Vec<package::Reference> = {
-            let installed_apps = ctx.installed_apps()?;
-            let manifest_paths = installed_apps.into_iter().filter_map(|path| {
-                let manifest_path = path.join("current").join("manifest.json");
+type ListApps<C> = Box<dyn Fn(&C) -> anyhow::Result<Option<Vec<package::Reference>>>>;
 
-                manifest_path
-                    .try_exists()
-                    .ok()
-                    .and_then(|exists| exists.then_some(manifest_path))
-            });
+pub struct AppsDecider<'c, C: ?Sized> {
+    ctx: &'c C,
+    all: ListApps<C>,
+    provided: Vec<package::Reference>,
+    collections: CollectionNames,
+}
 
-            let references = manifest_paths
-                .map(manifest::Reference::File)
-                .map(manifest::Reference::into_package_ref);
+impl<'c, C: ?Sized> AppsDecider<'c, C> {
+    pub fn new(ctx: &'c C, all: ListApps<C>, provided: Vec<package::Reference>) -> Self {
+        Self {
+            ctx,
+            all,
+            provided,
+            collections: CollectionNames::default(),
+        }
+    }
 
-            references.collect()
+    /// This will get all apps if all is true and no apps were passed.
+    ///
+    /// If all is false and apps are passed, it will only return those apps.
+    ///
+    /// If all is true and apps are passed, it will prompt the user to select which collection to choose.
+    ///
+    /// If the return value is empty, that means no apps were provided and all was false.
+    pub fn decide(self) -> anyhow::Result<Option<Vec<package::Reference>>> {
+        let Some(installed_apps) = (self.all)(self.ctx)? else {
+            return Ok(Some(self.provided));
         };
 
-        if apps.is_empty() {
+        if self.provided.is_empty() {
             Ok(Some(installed_apps))
         } else {
             let choices = [
@@ -47,8 +42,12 @@ pub fn handle_installed_apps(
                     installed_apps,
                 ),
                 (
-                    green!("Provided apps - {} (see command invocation)", apps.len()).to_string(),
-                    apps,
+                    green!(
+                        "Provided apps - {} (see command invocation)",
+                        self.provided.len()
+                    )
+                    .to_string(),
+                    self.provided,
                 ),
             ];
 
@@ -72,7 +71,20 @@ pub fn handle_installed_apps(
 
             Ok(Some(apps))
         }
-    } else {
-        Ok(Some(apps))
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct CollectionNames {
+    all: &'static str,
+    provided: &'static str,
+}
+
+impl Default for CollectionNames {
+    fn default() -> Self {
+        Self {
+            all: "All installed apps",
+            provided: "Provided apps",
+        }
     }
 }
